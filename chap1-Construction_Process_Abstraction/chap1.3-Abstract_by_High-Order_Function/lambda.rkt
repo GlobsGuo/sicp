a87b65c02917b21f70df514266fbc6320a59840f
GlobsGuo <578618446@qq.com>
2019-08-13 16:35:54 +0800

    chap1 final commit

---
+++ chap1-Construction_Process_Abstraction/chap1.3-Abstract_by_High-Order_Function/lambda.rkt
@@ -0,0 +1,7 @@
+#!/usr/bin/racket
+#lang sicp 
+(display (remainder 1 2))
+(newline)
+
+(cond ((= 1 1) 0)
+	  (else 2))
--- chap1-Construction_Process_Abstraction/intergration.rkt
+++ chap1-Construction_Process_Abstraction/intergration.rkt
@@ -1,4 +1,5 @@
 #lang sicp
+; 1.29
 (define (cube x) (* x x x))
 (define (simpson f a b n)
   (define h (/ (- b a) n))
@@ -6,10 +7,18 @@
     (if (> a b)
         0
         (+ (term a) (sum term (next a) next b))))
+  ; 1.30
+  (define (sum-iter term a next b)
+    (define (iter a result)
+      (if (> a b)
+          result
+          (iter (next a) (+ (term a) result))))
+    (iter a 0))
   (define (y k)
     (f(+ a (* k h))))
   (define (term k)
     (cond ((or (= k 0) (= k n)) (y k))
           ((even? k) (* 2 (y k)))
           (else (* 4 (y k)))))
-  (* (sum term 0 inc n) (/ h 3)))
+  (* (sum-iter term 0 inc n) (/ h 3)))
+
